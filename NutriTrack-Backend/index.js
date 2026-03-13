b7b22b8ac9ff89c3a7c5d54b93a1bee97fe46569
Anik Chakraborti <aneekneel1234@gmail.com>
2026-03-13 07:47:54 -0400

    Backend Updated

--- NutriTrack-Backend/index.js
+++ NutriTrack-Backend/index.js
@@ -44,7 +44,7 @@ app.listen(PORT, () => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/NutriTrack-Frontend/dist")));
 
-  app.get("*", (req, res) =>{
+  app.get(/.*/, (req, res) =>{
     res.sendFile(path.resolve(__dirname, "NutriTrack-Frontend", "dist", "index.html"));
   });
 }  
